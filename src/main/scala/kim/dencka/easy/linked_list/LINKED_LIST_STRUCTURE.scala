2119b9603e2bbddd8c35767b4d9ebbe82d6125cf
Denis Kim <densungkim@gmail.com>
2022-07-15 00:58:05 +0500

    [new] MiddleOfTheLinkedList PalindromeLinkedList [some changes]

--- src/main/scala/kim/dencka/easy/linked_list/LINKED_LIST_STRUCTURE.scala
+++ src/main/scala/kim/dencka/easy/linked_list/LINKED_LIST_STRUCTURE.scala
@@ -1,8 +1,37 @@
 package kim.dencka.easy.linked_list
 
 object LINKED_LIST_STRUCTURE {
-  class ListNode(val x: Int, val next: ListNode = null)
+  case class ListNode(val x: Int, val next: ListNode = null)
 
+  // 1,2,3,4,5,6,7,8,9
+  val list0 =
+    new ListNode(
+      1,
+      new ListNode(
+        2,
+        new ListNode(
+          3,
+          new ListNode(
+            4,
+            new ListNode(
+              5,
+              new ListNode(
+                6,
+                new ListNode(
+                  7,
+                  new ListNode(
+                    8,
+                    new ListNode(
+                      9
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
   // 1,2,3,6,4,5,6
   val list1 =
     new ListNode(
@@ -51,4 +80,35 @@ object LINKED_LIST_STRUCTURE {
   val list5 = new ListNode(4, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5)))))
   // 5,6,1,8,4,5
   val list6 = new ListNode(5, new ListNode(6, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5))))))
+  // 1,2,3,4,5,4,3,2,1
+  val list7 = new ListNode(
+    1,
+    new ListNode(
+      2,
+      new ListNode(
+        3,
+        new ListNode(
+          4,
+          new ListNode(
+            5,
+            new ListNode(
+              6,
+              new ListNode(
+                4,
+                new ListNode(
+                  3,
+                  new ListNode(
+                    2,
+                    new ListNode(
+                      1
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
 }
---
+++ src/main/scala/kim/dencka/easy/linked_list/PalindromeLinkedList.scala
@@ -0,0 +1,41 @@
+package kim.dencka.easy.linked_list
+
+import LINKED_LIST_STRUCTURE._
+
+object PalindromeLinkedList extends App {
+
+  def isPalindrome(head: ListNode): Boolean = {
+    @annotation.tailrec
+    def reverse(ln: ListNode, acc: ListNode): ListNode = ln match {
+      case null => acc
+      case _ => reverse(ln.next, new ListNode(ln.x, acc))
+    }
+
+    @annotation.tailrec
+    def compare(ln1: ListNode, ln2: ListNode): Boolean = (ln1, ln2) match {
+      case (null, null) => true
+      case (l1, l2) if l1 == null || l2 == null || l1.x != l2.x => false
+      case _ => compare(ln1.next, ln2.next)
+    }
+
+    compare(head, reverse(head, null))
+  }
+
+// TODO THERE IS SOLUTION FROM DISCUSSION (I LIKE IT)
+
+//  def isPalindrome(head: ListNode): Boolean = {
+//    def go(lhead: ListNode, node: ListNode): ListNode = {
+//      if (node == null) lhead
+//      else {
+//        val ln = go(lhead, node.next)
+//        if (ln == null || ln.x != node.x) null
+//        else if (ln.next != null) ln.next
+//        else ln
+//      }
+//    }
+//
+//    head == null || head.next == null || go(head, head) != null
+//  }
+
+
+}
