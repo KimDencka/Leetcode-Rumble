84cec27b74555c43f0626cf36d9cc7d85f5f34d5
Denis Kim <densungkim@gmail.com>
2022-07-15 20:56:06 +0500

    [new] RangeSumQuery_Immutable

---
+++ src/main/scala/kim/dencka/easy/collection/RangeSumQuery_Immutable.scala
@@ -0,0 +1,17 @@
+package kim.dencka.easy.collection
+
+object RangeSumQuery_Immutable extends App {
+
+  class NumArray(_nums: Array[Int]) {
+
+    def sumRange(left: Int, right: Int): Int = {
+      @annotation.tailrec
+      def run(l: Int, acc: Int): Int = l match {
+        case _ if l > right => acc
+        case _ => run(l + 1, acc + _nums(l))
+      }
+      run(left, 0)
+    }
+  }
+
+}
