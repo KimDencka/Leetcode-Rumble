9418e825d98e6ac27713ed44b23ba8da8ec856bd
Denis Kim <densungkim@gmail.com>
2022-07-22 13:16:20 +0500

    [new] PartitionList

--- src/main/scala/kim/dencka/payloads/LinkedListStructure.scala
+++ src/main/scala/kim/dencka/payloads/LinkedListStructure.scala
@@ -111,4 +111,24 @@ object LinkedListStructure {
       )
     )
   )
+  // 1,4,3,2,5,2
+  val list8 =
+    new ListNode(
+      1,
+      new ListNode(
+        4,
+        new ListNode(
+          3,
+          new ListNode(
+            2,
+            new ListNode(
+              5,
+              new ListNode(
+                2
+              )
+            )
+          )
+        )
+      )
+    )
 }
