39b6b2bc2d3bda39d37ebcf1c5657aec76a2185c
Denis Kim <densungkim@gmail.com>
2022-07-22 11:35:19 +0500

    [new] PowerOfFour

--- src/main/scala/kim/dencka/easy/other/PowerOfTwo.scala
+++ src/main/scala/kim/dencka/easy/other/PowerOfTwo.scala
@@ -2,9 +2,8 @@ package kim.dencka.easy.other
 
 object PowerOfTwo extends App {
 
-  def isPowerOfTwo(n: Int): Boolean = {
-    if (n<=0) false else (n & (n-1)) == 0
-  }
+  def isPowerOfTwo(n: Int): Boolean =
+    if (n <= 0) false else (n & (n - 1)) == 0
 
 //  def isPowerOfTwo2(n: Int): Boolean = {
 //    @annotation.tailrec
@@ -14,6 +13,12 @@ object PowerOfTwo extends App {
 //    val ones = hammingWeight(n)
 //    if (n < 0) false
 //    else ones == 1
+//  }
+
+//  def isPowerOfFour(n: Int): Boolean = {
+//    val exponent: Double = math.log10(n) / math.log10(2.0)
+//    if (exponent - math.round(exponent) == 0) true
+//    else false
 //  }
 
 }
