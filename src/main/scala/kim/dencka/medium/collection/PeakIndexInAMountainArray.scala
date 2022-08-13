43faa3bcd45de43bafa1c684eddd143e3a422fe1
Denis Kim <densungkim@gmail.com>
2022-08-13 13:48:40 +0500

    [new] PeakIndexInAMountainArray

---
+++ src/main/scala/kim/dencka/medium/collection/PeakIndexInAMountainArray.scala
@@ -0,0 +1,19 @@
+package kim.dencka.medium.collection
+
+object PeakIndexInAMountainArray extends App {
+  //    arr.indexOf(arr.max) // slow but elegant solution
+
+  def peakIndexInMountainArray(arr: Array[Int]): Int = {
+    @annotation.tailrec
+    def loop(low: Int, high: Int): Int = {
+      val mid = (low + high) / 2
+      arr(mid) match {
+        case num if num < arr(mid + 1) => loop(mid + 1, high)
+        case num if num > arr(mid + 1) && num > arr(mid - 1) => mid
+        case _ => loop(low, mid)
+      }
+    }
+    loop(0, arr.length - 1)
+  }
+
+}
