6cd174739afe73c2e0306878f9a3b953967b6217
Denis Kim <densungkim@gmail.com>
2022-08-16 22:59:49 +0500

    [new] MergeSortedArray (java)

---
+++ src/main/scala/kim/dencka/java/easy/collection/PascalsTriangle.java
@@ -0,0 +1,24 @@
+package kim.dencka.java.easy.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        System.out.println(generate(5));
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+            }
+            if (i > 0) row.add(1);
+            result.add(row);
+        }
+        return result;
+    }
+}
