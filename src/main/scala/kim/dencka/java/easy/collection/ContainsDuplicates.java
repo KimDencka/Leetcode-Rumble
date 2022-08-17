fed2f1ef0180a6b5b057b8b41e6a7a86998a9987
Denis Kim <densungkim@gmail.com>
2022-08-17 18:30:13 +0500

    [new] ContainsDuplicates (java)

---
+++ src/main/scala/kim/dencka/java/easy/collection/ContainsDuplicates.java
@@ -0,0 +1,26 @@
+package kim.dencka.java.easy.collection;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ContainsDuplicates {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        int[] nums2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.println(containsDuplicate(nums));
+        System.out.println(containsDuplicate(nums2));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, List.of(num));
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
