ae77761a139d2c9bb95d5ab72855765fa8d749bc
Denis Kim <densungkim@gmail.com>
2022-08-17 14:47:18 +0500

    [new] SingleNumber (java) [new] KthMissingPositiveNumber (java)

---
+++ src/main/scala/kim/dencka/java/easy/collection/KthMissingPositiveNumber.java
@@ -0,0 +1,36 @@
+package kim.dencka.java.easy.collection;
+
+public class KthMissingPositiveNumber {
+
+    // KIM_INFO THERE IS NO SCALA SOLUTION
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 4, 7, 11};
+        System.out.println(findKthPositive(nums, 5));
+    }
+
+    public static int findKthPositive(int[] arr, int k) {
+        for (int j : arr) {
+            if (k >= j) k++;
+            else break;
+        }
+        return k;
+    }
+
+    // binary search
+//    public static int findKthPositive(int[] arr, int k) {
+//        int start = 0;
+//        int end = arr.length;
+//        while (start < end) {
+//            int mid = start + (end - start) / 2;
+//            if (arr[mid] - mid - 1 < k) {
+//                start = mid + 1;
+//            } else {
+//                end = mid;
+//            }
+//        }
+//        return start + k;
+//    }
+
+
+}
