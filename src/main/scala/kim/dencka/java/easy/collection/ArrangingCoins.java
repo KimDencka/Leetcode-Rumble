6b6a1ecf364fd327f7a66ace506caac034db3289
Denis Kim <densungkim@gmail.com>
2022-08-17 15:31:02 +0500

    [new] ArrangingCoins (java)

---
+++ src/main/scala/kim/dencka/java/easy/collection/ArrangingCoins.java
@@ -0,0 +1,33 @@
+package kim.dencka.java.easy.collection;
+
+public class ArrangingCoins {
+
+    // KIM_INFO THERE IS NO SCALA SOLUTION
+
+    public static void main(String[] args) {
+        System.out.println(arrangeCoins(10));
+    }
+
+    public static int arrangeCoins(int n) {
+        long low = 0;
+        long high = n;
+        while (low <= high) {
+            long mid = (low + (high - low) / 2);
+            long curr = mid * (mid + 1) / 2;
+
+            if (curr == n) return (int) mid;
+
+            if (n < curr) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return (int) high;
+    }
+
+    // MATH
+//    public static int arrangeCoins(int n) {
+//        return (int)(Math.sqrt(2 * (long)n + 0.25) - 0.5);
+//    }
+}
