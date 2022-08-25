caa51ba0c28bef8ea0c4a7d4ea65adb236e4b87f
Denis Kim <densungkim@gmail.com>
2022-08-25 23:35:14 +0500

    [new] RemoveAllAdjacentDuplicatesInString (scala)

---
+++ src/main/scala/kim/dencka/scala/easy/other/RemoveAllAdjacentDuplicatesInString.scala
@@ -0,0 +1,18 @@
+package kim.dencka.scala.easy.other
+
+import scala.collection.mutable
+
+object RemoveAllAdjacentDuplicatesInString extends App {
+
+  def removeDuplicates(s: String): String =
+    s.foldLeft(mutable.Stack.empty[Char]) { (stack, ch) =>
+      if (stack.isEmpty) stack.push(ch)
+      else if (stack.top == ch) {
+        stack.pop()
+        stack
+      }
+      else stack.push(ch)
+    }.mkString.reverse
+
+  println(removeDuplicates("abbaca"))
+}
