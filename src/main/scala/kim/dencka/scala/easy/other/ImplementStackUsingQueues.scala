fcd1fa9d91f79a72b3ef49120cbf28b14817825b
Denis Kim <densungkim@gmail.com>
2022-09-05 14:10:56 +0500

    [new] ImplementStackUsingQueues (scala)

---
+++ src/main/scala/kim/dencka/scala/easy/other/ImplementStackUsingQueues.scala
@@ -0,0 +1,31 @@
+package kim.dencka.scala.easy.other
+import scala.collection.mutable
+
+object ImplementStackUsingQueues extends App {
+
+  class MyStack {
+    val q: mutable.Queue[Int] = scala.collection.mutable.Queue.empty[Int]
+
+    def push(x: Int): Unit = {
+      q.enqueue(x)
+      (0 until q.size - 1)
+        .foreach(_ => q.enqueue(q.dequeue()))
+    }
+
+    def pop(): Int = q.dequeue()
+
+    def top(): Int = q.front
+
+    def empty(): Boolean = q.isEmpty
+
+  }
+
+  /**
+   * Your MyStack object will be instantiated and called as such:
+   * var obj = new MyStack()
+   * obj.push(x)
+   * var param_2 = obj.pop()
+   * var param_3 = obj.top()
+   * var param_4 = obj.empty()
+   */
+}
