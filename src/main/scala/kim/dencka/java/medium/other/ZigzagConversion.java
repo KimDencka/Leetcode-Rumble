3f6e94431d8609aa637bcd516a851855f2a79d2b
Denis Kim <densungkim@gmail.com>
2022-09-14 21:31:35 +0500

    [new] ZigzagConversion

---
+++ src/main/scala/kim/dencka/java/medium/other/ZigzagConversion.java
@@ -0,0 +1,33 @@
+package kim.dencka.java.medium.other;
+
+import java.util.Arrays;
+
+class ZigzagConversion {
+    // KIM_INFO NOT SOLVED IN SCALA
+
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING", 3));
+    }
+
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        String[] chunks = new String[numRows];
+        Arrays.fill(chunks, "");
+        boolean switcher = true;
+        int i = 0;
+        StringBuilder res = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (switcher) {
+                chunks[i++] += ch;
+                if (i == numRows - 1) switcher = false;
+            } else {
+                chunks[i--] += ch;
+                if (i == 0) switcher = true;
+            }
+        }
+
+        for (String str : chunks) res.append(str);
+        return res.toString();
+    }
+}
