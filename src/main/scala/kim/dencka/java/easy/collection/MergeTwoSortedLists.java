7aa632bbd439a080314415efb3a6d804738902fa
Denis Kim <densungkim@gmail.com>
2022-09-22 17:33:38 +0500

    [new] MergeTwoSortedLists (java)

---
+++ src/main/scala/kim/dencka/java/easy/collection/MergeTwoSortedLists.java
@@ -0,0 +1,54 @@
+package kim.dencka.java.easy.collection;
+
+import kim.dencka.java.payloads.ListNode;
+
+class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        printAll(mergeTwoLists(list1, list2));
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode head, temp;
+
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+
+        temp = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (list1 == null) {
+            temp.next = list2;
+            return head;
+        }
+        temp.next = list1;
+        return head;
+    }
+
+    public static void printAll(ListNode list) {
+        while (list != null) {
+            System.out.print(list.val + " ");
+            list = list.next;
+        }
+        System.out.println();
+    }
+}
