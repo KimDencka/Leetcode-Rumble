27e010142c99223893ae3816b933974c8fb065c4
Denis Kim <densungkim@gmail.com>
2022-09-30 15:08:43 +0500

    [new] BestTimeToBuyAndSellStockWithCooldown (java)

---
+++ src/main/scala/kim/dencka/java/medium/collection/BestTimeToBuyAndSellStockWithCooldown.java
@@ -0,0 +1,21 @@
+package kim.dencka.java.medium.collection;
+
+class BestTimeToBuyAndSellStockWithCooldown {
+    // KIM_INFO NOT SOLVED IN SCALA
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{1, 2, 0, 2, 3, 0, 2}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int buy = -prices[0];
+        int old = 0;
+        int cur = 0;
+        for (int i = 1; i < prices.length; i++) {
+            buy = Math.max(old - prices[i], buy);
+            old = cur;
+            cur = Math.max(buy + prices[i], cur);
+        }
+        return cur;
+    }
+}
