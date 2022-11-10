e8c301a6ba7ae877dcd9d07b23ecff61b215b442
Denis Kim <densungkim@gmail.com>
2022-11-10 08:51:41 +0500

    [new] MinimumAdditionToMakeIntegerBeautiful (java)

---
+++ src/main/scala/kim/dencka/java/medium/collection/MinimumAdditionToMakeIntegerBeautiful.java
@@ -0,0 +1,22 @@
+package kim.dencka.java.medium.collection;
+
+class MinimumAdditionToMakeIntegerBeautiful {
+    public static void main(String[] args) {
+        long res = makeIntegerBeautiful(734504727, 10);
+        System.out.println(res);
+    }
+
+    public static long makeIntegerBeautiful(long n, int target) {
+        long n0 = n, base = 1;
+        while (getSum(n) > target) {
+            n = n / 10 + 1;
+            base *= 10;
+        }
+        return n * base - n0;
+    }
+
+    public static long getSum(long n) {
+        if (n < 10) return n;
+        return n % 10 + getSum(n / 10);
+    }
+}
