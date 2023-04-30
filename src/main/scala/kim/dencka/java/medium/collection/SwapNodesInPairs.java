e7e82b3170825cf49e51efa1795c23ba7db06109
Denis Kim <densungkim@gmail.com>
2023-04-30 05:24:43 +0500

    fix formatting

--- src/main/scala/kim/dencka/java/medium/collection/SwapNodesInPairs.java
+++ src/main/scala/kim/dencka/java/medium/collection/SwapNodesInPairs.java
@@ -4,7 +4,8 @@ import kim.dencka.java.payloads.ListNode;
 
 class SwapNodesInPairs {
     public static void main(String[] args) {
-        ListNode input = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8, new ListNode(9)))))))));        printAll(swapPairs(input));
+        ListNode input = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8, new ListNode(9)))))))));
+        printAll(swapPairs(input));
     }
 
     public static ListNode swapPairs(ListNode head) {
