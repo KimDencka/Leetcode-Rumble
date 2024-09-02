73fc66ca446c8590f9cb822bbcc96a5357c67ff1
Denis Kim <densungkim@gmail.com>
2024-09-02 17:24:37 +0500

    [new] Kids With the Greatest Number of Candies (scala)

---
+++ src/main/scala/kim/dencka/scala/easy/collection/KidsWithTheGreatestNumberOfCandies.scala
@@ -0,0 +1,17 @@
+package kim.dencka.scala.easy.collection
+
+object KidsWithTheGreatestNumberOfCandies extends App {
+
+  def kidsWithCandies(candies: Array[Int], extraCandies: Int): List[Boolean] = {
+    val max    = candies.max
+    val result = Array.ofDim[Boolean](candies.length)
+    result.indices.foreach { i =>
+      if (max <= candies(i) + extraCandies) result(i) = true
+    }
+    result.toList
+  }
+
+  println(kidsWithCandies(Array(2, 3, 5, 1, 3), 3)) // output: List(true,true,true,false,true)
+  println(kidsWithCandies(Array(4, 2, 1, 1, 2), 1)) // output: List(true,false,false,false,false)
+
+}
