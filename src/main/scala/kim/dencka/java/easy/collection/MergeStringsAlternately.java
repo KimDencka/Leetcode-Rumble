4dd002867fed16bc8413f1a97206d44bddb23d13
Denis Kim <densungkim@gmail.com>
2025-09-01 15:13:20 +0500

    [additional] Merge Strings Alternately (java)

--- src/main/scala/kim/dencka/java/easy/collection/MergeStringsAlternately.java
+++ src/main/scala/kim/dencka/java/easy/collection/MergeStringsAlternately.java
@@ -14,22 +14,33 @@ public class MergeStringsAlternately {
     }
 
     public static String mergeAlternately(String word1, String word2) {
-        StringBuilder sb = new StringBuilder();
-        int len1 = word1.length();
-        int len2 = word2.length();
-
-        int minLen = Math.min(len1, len2);
-        for (int i = 0; i < minLen; i++) {
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(i));
-            if (i == minLen - 1) {
-                if (len1 < len2) {
-                    sb.append(word2.substring(i + 1));
-                } else {
-                    sb.append(word1.substring(i + 1));
-                }
-            }
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) result.append(word1.charAt(i));
+            if (i < word2.length()) result.append(word2.charAt(i));
+            i++;
         }
-        return sb.toString();
+        return result.toString();
     }
+
+//    public static String mergeAlternately(String word1, String word2) {
+//        StringBuilder sb = new StringBuilder();
+//        int len1 = word1.length();
+//        int len2 = word2.length();
+//
+//        int minLen = Math.min(len1, len2);
+//        for (int i = 0; i < minLen; i++) {
+//            sb.append(word1.charAt(i));
+//            sb.append(word2.charAt(i));
+//            if (i == minLen - 1) {
+//                if (len1 < len2) {
+//                    sb.append(word2.substring(i + 1));
+//                } else {
+//                    sb.append(word1.substring(i + 1));
+//                }
+//            }
+//        }
+//        return sb.toString();
+//    }
 }
