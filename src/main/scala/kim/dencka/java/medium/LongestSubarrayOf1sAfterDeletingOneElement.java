956e9daf41611fa68c7b10be2b73c2c5064db26a
Denis Kim <densungkim@gmail.com>
2025-09-04 15:25:18 +0500

    [new] Longest Subarray of 1's After Deleting One Element (java)

---
+++ src/main/scala/kim/dencka/java/medium/LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -0,0 +1,23 @@
+package kim.dencka.java.medium;
+
+public class LongestSubarrayOf1sAfterDeletingOneElement {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 1, 1, 0, 1, 1, 0, 1};
+        System.out.println(longestSubarray(nums)); // 5
+    }
+
+    public static int longestSubarray(int[] nums) {
+        System.gc();
+        int l = 0, zeros = 0;
+        int result = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] == 0) zeros++;
+            if (zeros > 1) {
+                if (nums[l] == 0) zeros--;
+                l++;
+            }
+            result = Math.max(result, r - l);
+        }
+        return result;
+    }
+}
