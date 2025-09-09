fc344760338826ccb0ed6a4bee4cb3d6dcac164e
Denis Kim <densungkim@gmail.com>
2025-09-09 13:08:38 +0500

    [new] Maximum Depth of Binary Tree (java)

---
+++ src/main/scala/kim/dencka/java/easy/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,20 @@
+package kim.dencka.java.easy;
+
+import kim.dencka.java.payloads.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(3);
+        tree.left = new TreeNode(9);
+        tree.right = new TreeNode(20);
+        tree.right.left = new TreeNode(15);
+        tree.right.right = new TreeNode(7);
+        System.out.println(maxDepth(tree)); // 3
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
