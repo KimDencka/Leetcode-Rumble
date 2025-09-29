3184090d438f50e706baa6b3f2b36dea99ada8d6
Denis Kim <densungkim@gmail.com>
2025-09-29 19:16:42 +0500

    [new] Minimum Number of Arrows to Burst Balloons (java)

---
+++ src/main/scala/kim/dencka/java/medium/MinimumNumberOfArrowsToBurstBallons.java
@@ -0,0 +1,26 @@
+package kim.dencka.java.medium;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MinimumNumberOfArrowsToBurstBallons {
+    public static void main(String[] args) {
+
+    }
+
+    public static int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
+
+        int shots = 1;
+        int prevEnd = points[0][1];
+
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > prevEnd) {
+                shots++;
+                prevEnd = points[i][1];
+            }
+        }
+
+        return shots;
+    }
+}
